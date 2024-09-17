160724d9f005182d549866136192b642412c1f5a
Faisal Ahammod <140565581+BinaryFaisal@users.noreply.github.com>
2024-09-18 00:54:52 +0600

    Donor not found section added

--- script.js
+++ script.js
@@ -149,26 +149,6 @@ document.getElementById('district').addEventListener('change', function () {
     }
 });
 
-// Form submission logic and popup handling
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission for demonstration purposes
-
-    // Show the popup
-    var popup = document.getElementById('popup');
-    popup.style.display = 'block';
-
-    // Close popup when "X" is clicked
-    // document.getElementById('closePopup').onclick = function () {
-    //     popup.style.display = 'none';
-    // };
-
-    // Close popup if the user clicks outside the popup content
-    // window.onclick = function (event) {
-    //     if (event.target === popup) {
-    //         popup.style.display = 'none';
-    //     }
-    // };
-});
 
 // Script for Navigation bar
 let menuList = document.getElementById("menuList");
